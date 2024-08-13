8b3b7349622493798155528bae7e3e06f0a10b2a
rtthw <172864013+rtthw@users.noreply.github.com>
2024-08-12 21:37:05 -0400

    ...

--- src/main.rs
+++ src/main.rs
@@ -1,114 +1,39 @@
-use leptos::*;
-use leptos_router::*;
+
+use dioxus::prelude::*;
 
 
 
 fn main() {
-    mount_to_body(|| view! { <App/> });
+    launch(app);
+}
+
+fn app() -> Element {
+    rsx! { Router::<Route> {} }
+}
+
+
+
+#[derive(Routable, Clone)]
+enum Route {
+    #[route("/")]
+    Home {},
+    // PageNotFound is a catch all route that will match any route and placing the matched segments in the route field
+    #[route("/:..route")]
+    PageNotFound { route: Vec<String> },
 }
 
 
 
 #[component]
-pub fn app() -> impl IntoView {
-    // Provides context that manages stylesheets, titles, meta tags, etc.
-    leptos_meta::provide_meta_context();
-    provide_context(GlobalState::new());
-
-    view! {
-        <Router>
-
-            <nav>
-            </nav>
-
-            <header></header>
-
-            <main class="container">
-                <SideNav/>
-                <Routes>
-                    <Route path="/blog" view=Blog/>
-                    // <Route path="#blog/:id" view=Post/>
-                    <Route path="/about" view=About/>
-                    <Route path="*" view=PageNotFound/>
-                </Routes>
-            </main>
-
-            <footer></footer>
-
-        </Router>
-    }
-}
-
-#[derive(Copy, Clone, Debug)]
-struct GlobalState {
-    show_sidenav: RwSignal<bool>,
-}
-
-impl GlobalState {
-    pub fn new() -> Self {
-        Self {
-            show_sidenav: create_rw_signal(false),
-        }
-    }
-}
-
-
-
-// ================================================================================================
-
-
-
-#[component]
-pub fn side_nav() -> impl IntoView {
-    let state = use_context::<GlobalState>().unwrap();
-    provide_context(state.clone());
-    let hidden = !state.show_sidenav.get();
-    view! {
-        <div id="sidenav" class:sidenav_hidden=hidden class:sidenav_shown=state.show_sidenav>
-            <button 
-                id="sidenav-close" 
-                on:click=move |_| state.show_sidenav.update(|v| *v = !*v)
-            >
-                X
-            </button>
-            <A exact=true href="/">"Home"</A>
-            <A href="/blog">"Blog"</A>
-            <A href="/about">"About"</A>
-        </div>
-    }
+fn Home() -> Element {
+    rsx! { h1 { "Home" } }
 }
 
 #[component]
-pub fn home() -> impl IntoView {
-    let state = use_context::<GlobalState>().unwrap();
-    provide_context(state.clone());
-    view! {
-        <p>Home</p>
-        <button 
-            on:click=move |_| state.show_sidenav.update(|v| *v = !*v)
-        >
-            "X"
-        </button>
-    }
-}
-
-#[component]
-pub fn blog() -> impl IntoView {
-    view! {
-        <p>Blog</p>
-    }
-}
-
-#[component]
-pub fn about() -> impl IntoView {
-    view! {
-        <p>About</p>
-    }
-}
-
-#[component]
-pub fn page_not_found() -> impl IntoView {
-    view! {
-        <Redirect path="/"/>
+fn PageNotFound(route: Vec<String>) -> Element {
+    rsx! {
+        h1 { "Page not found" }
+        p { "The page you requested doesn't exist." }
+        pre { color: "red", "log:\nattemped to navigate to: {route:?}" }
     }
 }

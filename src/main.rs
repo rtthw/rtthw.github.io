9f05b36825f0703bc23e2ec1c0cd9acbf7c814ba
rtthw <172864013+rtthw@users.noreply.github.com>
2024-09-06 07:42:44 -0400

    ...

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 
 
 
-const RAW_SITE_DATA_URL: &str = "https://raw.githubusercontent.com/rtthw/data/master/site-data";
+use eframe::egui;
+
+
+
+// const RAW_SITE_DATA_URL: &str = "https://raw.githubusercontent.com/rtthw/data/master/site-data";
 
 
 
@@ -9,10 +13,49 @@ const RAW_SITE_DATA_URL: &str = "https://raw.githubusercontent.com/rtthw/data/ma
 
 
 
+#[cfg(not(target_arch = "wasm32"))]
+fn main() -> eframe::Result<()> {
+    eframe::run_native(
+        "rtthw.github.io",
+        Default::default(),
+        Box::new(|cc| Ok(Box::new(Website::new(cc)))),
+    )
+}
+
+// when compiling to web using trunk.
+#[cfg(target_arch = "wasm32")]
 fn main() {
-    println!("Moving to egui...");
+    wasm_bindgen_futures::spawn_local(async {
+        eframe::WebRunner::new().start(
+            "canvas",
+            eframe::WebOptions::default(),
+            Box::new(|cc| Ok(Box::new(Website::new(cc)))),
+        )
+        .await
+        .expect("failed to start eframe");
+    });
 }
 
 
 
 // ================================================================================================
+
+
+
+struct Website {}
+
+impl eframe::App for Website {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.centered_and_justified(|ui| {
+                ui.heading("Coming Soon...");
+            });
+        });
+    }
+}
+
+impl Website {
+    pub fn new(_cc: &eframe::CreationContext) -> Self {
+        Self {}
+    }
+}

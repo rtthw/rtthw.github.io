e1e9530b399cc675c150073bd1a28965fe9cbedb
rtthw <172864013+rtthw@users.noreply.github.com>
2024-10-10 18:05:01 -0400

    Initial deployment test

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,54 @@
+use eframe::egui;
 
 
 
+
+#[cfg(not(target_arch = "wasm32"))]
+fn main() { todo!("native implementation") }
+
+#[cfg(target_arch = "wasm32")]
 fn main() {
-    println!("Hello, world!");
+    use wasm_bindgen::JsCast;
+    let web_options = eframe::WebOptions::default();
+    let element = eframe::web_sys::window()
+        .expect("failed to get window")
+        .document()
+        .expect("failed to get document")
+        .get_element_by_id("canvas")
+        .expect("failed to get canvas element")
+        .dyn_into::<eframe::web_sys::HtmlCanvasElement>()
+        .unwrap();
+    wasm_bindgen_futures::spawn_local(async {
+        eframe::WebRunner::new()
+            .start(
+                element,
+                web_options,
+                Box::new(|cc| Ok(Box::new(Program::new(cc)))),
+            )
+            .await
+            .expect("failed to start eframe");
+    });
+}
+
+
+
+struct Program;
+
+impl eframe::App for Program {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default()
+            .frame(egui::Frame::none()
+                .fill(egui::Color32::from_hex("#1e1f22").unwrap()))
+            .show(ctx, |ui| {
+                ui.centered_and_justified(|ui| {
+                    ui.heading("Coming Soon");
+                });
+            });
+    }
+}
+
+impl Program {
+    fn new(_cc: &eframe::CreationContext) -> Self {
+        Self {}
+    }
 }

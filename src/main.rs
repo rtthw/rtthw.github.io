e2fcc36651ae7768901a92941b4f7a62e19935df
rtthw <172864013+rtthw@users.noreply.github.com>
2024-10-11 06:03:27 -0400

    Setup simple routing system

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,10 @@ fn main() {
 
 
 
-struct Program;
+struct Program {
+    router: egui_router::EguiRouter<State>,
+    state: State,
+}
 
 impl eframe::App for Program {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
@@ -40,15 +43,74 @@ impl eframe::App for Program {
             .frame(egui::Frame::none()
                 .fill(egui::Color32::from_hex("#1e1f22").unwrap()))
             .show(ctx, |ui| {
-                ui.centered_and_justified(|ui| {
-                    ui.heading("Coming Soon");
-                });
+                self.router.ui(ui, &mut self.state);
             });
     }
 }
 
 impl Program {
     fn new(_cc: &eframe::CreationContext) -> Self {
-        Self {}
+        let mut state = State {};
+        let router = egui_router::EguiRouter::builder()
+            .history({
+                #[cfg(target_arch = "wasm32")]
+                let history = egui_router::history::BrowserHistory::new(Some("".to_string()));
+                #[cfg(not(target_arch = "wasm32"))]
+                let history = egui_router::history::DefaultHistory::default();
+                history
+            })
+            .transition(egui_router::TransitionConfig::fade())
+            .default_duration(0.3)
+            .default_path("/")
+            .route("/", home_route)
+            .route("/dreg/{slug}", dreg_route)
+            .build(&mut state);
+
+
+        Self {
+            router,
+            state,
+        }
+    }
+}
+
+
+
+pub struct State {}
+
+
+
+pub fn home_route(_req: egui_router::Request<State>) -> impl egui_router::Route<State> {
+    move |ui: &mut egui::Ui, _state: &mut State| {
+        ui.centered_and_justified(|ui| {
+            ui.heading("Home");
+        });
+    }
+}
+
+pub fn dreg_route(req: egui_router::Request<State>) -> impl egui_router::Route<State> {
+    let slug = req.params.get("slug").unwrap();
+    match slug {
+        "something" => {
+            move |ui: &mut egui::Ui, _state: &mut State| {
+                ui.centered_and_justified(|ui| {
+                    ui.heading("dreg/something");
+                });
+            }
+        }
+        "nothing" => {
+            move |ui: &mut egui::Ui, _state: &mut State| {
+                ui.centered_and_justified(|ui| {
+                    ui.heading("dreg/nothing");
+                });
+            }
+        }
+        _ => {
+            move |ui: &mut egui::Ui, _state: &mut State| {
+                ui.centered_and_justified(|ui| {
+                    ui.heading("dreg");
+                });
+            }
+        }
     }
 }

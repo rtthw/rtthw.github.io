d29e995f772de0213d94008198777df812085d45
rtthw <172864013+rtthw@users.noreply.github.com>
2024-10-24 08:38:19 -0400

    Move to dreg

--- src/main.rs
+++ src/main.rs
@@ -1,100 +1,60 @@
 
-mod dreg;
 
-use eframe::egui;
+
+use dreg::prelude::*;
 
 
 
 #[cfg(not(target_arch = "wasm32"))]
-fn main() { todo!("native implementation") }
+fn main() -> Result<()> {
+    let program = Website {};
+    let platform = CrosstermPlatform::new()?;
+    run_program(program, platform)?;
+    Ok(())
+}
 
 #[cfg(target_arch = "wasm32")]
-fn main() {
-    use wasm_bindgen::JsCast;
-    let web_options = eframe::WebOptions::default();
-    let element = eframe::web_sys::window()
-        .expect("failed to get window")
-        .document()
-        .expect("failed to get document")
-        .get_element_by_id("canvas")
-        .expect("failed to get canvas element")
-        .dyn_into::<eframe::web_sys::HtmlCanvasElement>()
-        .unwrap();
-    wasm_bindgen_futures::spawn_local(async {
-        eframe::WebRunner::new()
-            .start(
-                element,
-                web_options,
-                Box::new(|cc| Ok(Box::new(Program::new(cc)))),
-            )
-            .await
-            .expect("failed to start eframe");
-    });
+fn main() -> Result<()> {
+    let program = Website {};
+    let platform = WasmPlatform::new();
+    run_program(program, platform).unwrap();
+    Ok(())
 }
 
 
 
-struct Program {
-    router: egui_router::EguiRouter<State>,
-    state: State,
-}
+struct Website {}
 
-impl eframe::App for Program {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default()
-            .frame(egui::Frame::none()
-                .outer_margin(0.0)
-                .inner_margin(0.0)
-                .fill(egui::Color32::from_hex("#1e1f22").unwrap()))
-            .show(ctx, |ui| {
-                self.router.ui(ui, &mut self.state);
-            });
+impl Program for Website {
+    fn update(&mut self, frame: Frame) {
+        let label_area = frame.area.inner_centered(5, 1);
+        frame.buffer.set_stringn(
+            label_area.x,
+            label_area.y,
+            "rtthw",
+            5,
+            Style::new().add_modifier(Modifier::BOLD),
+        );
     }
-}
 
-impl Program {
-    fn new(_cc: &eframe::CreationContext) -> Self {
-        let mut state = State {
-            slug: "".to_string(),
-            // TODO: Scale font size on zoom.
-            font_size: 19.0,
-        };
-        let router = egui_router::EguiRouter::builder()
-            .history({
-                #[cfg(target_arch = "wasm32")]
-                let history = egui_router::history::BrowserHistory::new(Some("".to_string()));
-                #[cfg(not(target_arch = "wasm32"))]
-                let history = egui_router::history::DefaultHistory::default();
-                history
-            })
-            .transition(egui_router::TransitionConfig::fade())
-            .default_duration(0.3)
-            .default_path("/")
-            .route("/", home_route)
-            .route("/dreg", dreg::route)
-            .route("/dreg/{slug}", dreg::route)
-            .build(&mut state);
-
-        Self {
-            router,
-            state,
+    fn on_input(&mut self, input: Input) {
+        match input {
+            _ => {}
         }
     }
-}
 
-
-
-pub struct State {
-    pub slug: String,
-    pub font_size: f32,
-}
-
-
-
-pub fn home_route(_req: egui_router::Request<State>) -> impl egui_router::Route<State> {
-    move |ui: &mut egui::Ui, _state: &mut State| {
-        ui.centered_and_justified(|ui| {
-            ui.heading("Home");
-        });
+    fn on_platform_request(&self, request: &str) -> Option<&str> {
+        Some(match request {
+            "font" => "29px Hack, monospace",
+            "font_size" => "29",
+            "web::default_fg_style" => "#bcbec4",
+            "web::default_bg_style" => "#1e1f22",
+            _ => {
+                return None;
+            }
+        })
     }
+
+    // Exiting the website is obviously handled by the browser itself, no need to handle it.
+    fn should_exit(&self) -> bool { false }
 }

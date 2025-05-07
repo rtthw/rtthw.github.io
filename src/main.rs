139f9492dfaddb1e8c1c6c101a32ee5aab3155f2
rtthw <172864013+rtthw@users.noreply.github.com>
2025-05-07 06:31:13 -0400

    Setup WASM application

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,32 @@
+#![recursion_limit = "1024"]
+
+
+
+use web_sys::window;
 
 
 
 fn main() {
-    println!("Hello, world!");
+    console_error_panic_hook::set_once();
+    visit();
+}
+
+fn visit() {
+    let document = document();
+    let location = document.location().unwrap();
+    let body = body(&document);
+    let text_node = document.create_text_node(&location.pathname().unwrap());
+    body.append_child(text_node.as_ref()).expect("failed to append text");
+}
+
+
+
+fn document() -> web_sys::Document {
+    window()
+        .and_then(|win| win.document())
+        .expect("could not access document")
+}
+
+fn body(doc: &web_sys::Document) -> web_sys::HtmlElement {
+    doc.body().expect("could not access document.body")
 }
